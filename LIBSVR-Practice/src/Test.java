3a8039090b2e0854e33be1207889c287bec4ed3f
Sunwoo Yim <sunny8751@gmail.com>
2015-06-29 14:12:02 -0400

    Deleted unnecessary code.

--- LIBSVR-Practice/src/Test.java
+++ LIBSVR-Practice/src/Test.java
@@ -1,14 +1,7 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
 
 public class Test {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args){
 		
 	}
 }
